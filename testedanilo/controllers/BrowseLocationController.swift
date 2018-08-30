43bf3701dbbf25023f7f340739fde3c92c3f420f
mosyle <mosyle@mosyles-macbook-air.local>
2018-08-30 08:51:04 -0300

    Activity indicator when loading web view.

--- testedanilo/controllers/BrowseLocationController.swift
+++ testedanilo/controllers/BrowseLocationController.swift
@@ -9,39 +9,61 @@
 import UIKit
 import WebKit
 
-class BrowseLocationController : UIViewController, WKUIDelegate {
+class BrowseLocationController : UIViewController, WKUIDelegate, WKNavigationDelegate {
     
     var webView: WKWebView!
     
     var currentCity : City = City(id: 0, name: "Default City", country: "DC", lat: 0, lon: 0)
     
+    var activityIndicator : UIActivityIndicatorView = UIActivityIndicatorView()
+    
     override func loadView() {
         super.loadView()
         
         let webConfiguration = WKWebViewConfiguration()
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView = WKWebView(frame: view.frame, configuration: webConfiguration)
         webView.uiDelegate = self
-        view = webView
+        webView.navigationDelegate = self
+        view.addSubview(webView)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print("BrowseLocationController viewDidLoad()")
         
         webView.frame = view.bounds
         
         self.title = currentCity.getNameAndCountry()
         
         var urlComponents : URLComponents = URLComponents(string: "https://www.google.com/maps/search/")!
-        //        urlComponents.scheme = "https"
-        //        urlComponents.host = "google.com/maps/search/"
         
         urlComponents.queryItems = [URLQueryItem(name: "api", value: "1"),
                                     URLQueryItem(name: "query", value: "\(currentCity.lat),\(currentCity.lon)")]
         
+        //        urlComponents.scheme = "https"
+        //        urlComponents.host = "google.com/maps/search/"
         //        let myURL = URL(string: "https://www.google.com/maps/search/?api=1&query=\(currentCity.lat),\(currentCity.lon)")
+        
         let myRequest = URLRequest(url: urlComponents.url!)
         webView.load(myRequest)
+        
+        startActivityIndicator()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        stopActivityIndicator()
+    }
+    
+    func startActivityIndicator() {
+        activityIndicator.center = self.view.center 
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        view.addSubview(activityIndicator)
+        view.bringSubview(toFront: activityIndicator)
+        
+        activityIndicator.startAnimating()
+    }
+    
+    func stopActivityIndicator() {
+        activityIndicator.stopAnimating()
     }
 }
